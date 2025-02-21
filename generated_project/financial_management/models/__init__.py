5213325688a2e8df7952f76101e1e28fd6dfde69
Zeeshan Rasheed <zeeshan.rasheed@tuni.fi>
2025-02-21 15:17:44 +0200

    Updated .gitignore and removed generated_project from Git tracking

--- generated_project/financial_management/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .financial_models import Income, Expense, Savings
-from ..database.database import db
-
-# Make models available at package level
-__all__ = ['Income', 'Expense', 'Savings', 'db']
--- generated_project/financial_management/services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Initialize services module for financial management application.
-Makes financial services available for import throughout the application.
-"""
-
-from .financial_service import FinancialService
-
-__all__ = ['FinancialService']
